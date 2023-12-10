3af150f1616ea23b7b8901e17b60e93197b3191f
Md Tariq Ansari <tariq19ansari@gmail.com>
2023-12-10 01:17:10 -0800

    Intial commit with folder structure

---
+++ src/main/java/com/springboot/demo/UrlShortenerServicesApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlShortenerServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrlShortenerServicesApplication.class, args);
+	}
+
+}
